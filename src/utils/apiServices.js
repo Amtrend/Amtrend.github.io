df6a8a84bb48c24adeb7a67f2ed5da58bd87bb5e
amtrend <alexandermaltsev@bk.ru>
2026-04-02 18:36:47 +0300

    feature: changing the service for obtaining user information

--- src/utils/apiServices.js
+++ src/utils/apiServices.js
@@ -11,7 +11,7 @@ export const fetchWeather = async (city) => {
 
 export const fetchUserLocation = async () => {
     try {
-        const response = await fetch('http://ip-api.com/json');
+        const response = await fetch('https://ipapi.co/json');
         const data = await response.json();
         return data.city?.toLowerCase() || 'remote';
     } catch {
