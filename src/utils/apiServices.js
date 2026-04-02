b14c53be061f26081caf7b26d4f64640a9f6bbfc
amtrend <alexandermaltsev@bk.ru>
2026-04-02 19:24:58 +0300

    feature: update ip service

--- src/utils/apiServices.js
+++ src/utils/apiServices.js
@@ -11,7 +11,7 @@ export const fetchWeather = async (city) => {
 
 export const fetchUserLocation = async () => {
     try {
-        const response = await fetch('https://ipwho.is');
+        const response = await fetch('https://ipapi.co/json');
         const data = await response.json();
         return data.city?.toLowerCase() || 'remote';
     } catch {
